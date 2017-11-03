fed4ed3c62fc9c52bb2ba6cf4cb7b425555a942c
Andrew <dahl0665@umn.edu>
2017-11-02 20:11:53 -0500

    This code does not work on QCEW calls. Tried adding a variable to return the data series area, but that didn't work. Change the series back to a LAUS series and the basic structure works.

--- bls_qcew_call.py
+++ bls_qcew_call.py
@@ -2,18 +2,17 @@ def retrieve_bls():
     import requests
     import json
     headers = {'Content-type': 'application/json'}
-    data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
+    data = json.dumps({"seriesid":["LAUCT274300000000003"],"startyear":"2011", "endyear":"2014"})
     p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
     json_data = json.loads(p.text)
     for series in json_data['Results']['series']:
-        x=["series id","year","period","value","footnotes"]
+        x=["series id","area","year","period","value"]
         seriesId = series['seriesID']
         for item in series['data']:
             year = item['year']
             period = item['period']
             value = item['value']
-            if 'M01' <= period <= 'M12':
-                    x.append([seriesId,year,period,value])
+            x.append([seriesId,year,period,value])
     return x
 
 def main():
