525e98376e08e4b1178a2e107cf51276467fa82e
Andrew <dahl0665@umn.edu>
2017-12-05 08:41:38 -0600

    Added footnotes column.

--- bls_qcew_call.py
+++ bls_qcew_call.py
@@ -4,19 +4,23 @@ def retrieve_bls():
     import datetime
     thisyear = datetime.date.today().year
     headers = {'Content-type': 'application/json'}
-    data = json.dumps({"seriesid":["LAUCT274300000000003"],"startyear":thisyear-9, "endyear":thisyear})
+    data = json.dumps({"seriesid":['LAUMT273346000000003'],"startyear":thisyear-9, "endyear":thisyear})
     p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
     json_data = json.loads(p.text)
     outfile = 'results.txt'
     output = open(outfile,'w')
-    output.write('series,year,period,value' + '\n')
+    output.write('series,year,period,value,footnote' + '\n')
     for series in json_data['Results']['series']:
         seriesId = series['seriesID']
         for item in series['data']:
             year = item['year']
             period = item['period']
             value = item['value']
-            output.write(seriesId + ',' + year + ',' + period +',' + value + '\n')
+            footnotes=""
+            for footnote in item['footnotes']:
+                if footnote:
+                    footnotes = footnotes + footnote['text'] + ','
+            output.write(seriesId + ',' + year + ',' + period +',' + value + ',' + footnotes[0:-1] + '\n')
     output.close()
 
 def main():
