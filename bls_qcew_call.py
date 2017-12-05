4b7ce435022d5a7cc2d0b9cb9df76627b28b8fe2
Andrew <dahl0665@umn.edu>
2017-12-04 21:04:07 -0600

    Writes values from one series for past 9 years from current year to file.

--- bls_qcew_call.py
+++ bls_qcew_call.py
@@ -1,22 +1,26 @@
 def retrieve_bls():
     import requests
     import json
+    import datetime
+    thisyear = datetime.date.today().year
     headers = {'Content-type': 'application/json'}
-    data = json.dumps({"seriesid":["LAUCT274300000000003"],"startyear":"2011", "endyear":"2014"})
+    data = json.dumps({"seriesid":["LAUCT274300000000003"],"startyear":thisyear-9, "endyear":thisyear})
     p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
     json_data = json.loads(p.text)
+    outfile = 'results.txt'
+    output = open(outfile,'w')
+    output.write('series,year,period,value' + '\n')
     for series in json_data['Results']['series']:
-        x=["series id","area","year","period","value"]
         seriesId = series['seriesID']
         for item in series['data']:
             year = item['year']
             period = item['period']
             value = item['value']
-            x.append([seriesId,year,period,value])
-    return x
+            output.write(seriesId + ',' + year + ',' + period +',' + value + '\n')
+    output.close()
 
 def main():
-    print(retrieve_bls())
+    retrieve_bls()
 
 if __name__ == '__main__':
     main()
