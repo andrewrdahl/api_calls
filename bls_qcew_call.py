8d46b7e352914ba34dbdf9153a7f13327fd6f1d8
Andrew <dahl0665@umn.edu>
2017-12-05 20:33:58 -0600

    Holly shit, got the area name added!

--- bls_qcew_call.py
+++ bls_qcew_call.py
@@ -2,6 +2,16 @@ def retrieve_bls():
     import requests
     import json
     import datetime
+    import urllib.request
+    bls_areas = urllib.request.urlopen('https://download.bls.gov/pub/time.series/la/la.area')
+    area_string = []
+    for a in bls_areas.readlines():
+        line_decode = a.decode()
+        line = line_decode.split('\t')
+        area_string.append(line)
+        area_dict = {}
+        for a in area_string:
+            area_dict[a[1]] = a[2]
     thisyear = datetime.date.today().year
     headers = {'Content-type': 'application/json'}
     data = json.dumps({"seriesid":['LAUMT273346000000003'],"startyear":thisyear-9, "endyear":thisyear})
@@ -16,12 +26,14 @@ def retrieve_bls():
             year = item['year']
             period = item['period']
             value = item['value']
+            area = area_dict[seriesId[3:18]]
             footnotes=""
             for footnote in item['footnotes']:
                 if footnote:
                     footnotes = footnotes + footnote['text'] + ','
-            output.write(seriesId + ',' + year + ',' + period +',' + value + ',' + footnotes[0:-1] + '\n')
+            output.write(seriesId + ',' + area + ',' + year + ',' + period +',' + value + ',' + footnotes[0:-1] + '\n')
     output.close()
+    print(area_dict)
 
 def main():
     retrieve_bls()
