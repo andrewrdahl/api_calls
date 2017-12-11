ad86b85d6c74ba81ea694f12848e507ced21b385
andrewrdahl <33045310+andrewrdahl@users.noreply.github.com>
2017-12-11 10:27:57 -0600

    Update bls_laus_call.py
    Replace get_series() with list of series to call, to avoid the use of an external table. Added iterator for calling all years from 2000 to present.

--- bls_laus_call.py
+++ bls_laus_call.py
@@ -7,50 +7,97 @@ def retrieve_bls():
     area_dict = get_areas()
     area_type_dict = {'MT':'Metropolitan Area','CT':'City','ST':'State','CN':'County'}
     measure_type_dict = get_measure_type()
-    series_list = get_series()
+    # Series IDs can be found at https://www.bls.gov/lau/
+    series_list = [
+        'LAUMT273346000000003',
+        'LAUMT273346000000004',
+        'LAUMT273346000000005',
+        'LAUMT273346000000006',
+        'LAUCT274300000000003',
+        'LAUCT274300000000004',
+        'LAUCT274300000000005',
+        'LAUCT274300000000006',
+        'LAUCT275800000000003',
+        'LAUCT275800000000004',
+        'LAUCT275800000000005',
+        'LAUCT275800000000006',
+        'LAUST270000000000003',
+        'LAUST270000000000004',
+        'LAUST270000000000005',
+        'LAUST270000000000006',
+        'LAUCN270030000000003',
+        'LAUCN270030000000004',
+        'LAUCN270030000000005',
+        'LAUCN270030000000006',
+        'LAUCN270190000000003',
+        'LAUCN270190000000004',
+        'LAUCN270190000000005',
+        'LAUCN270190000000006',
+        'LAUCN270370000000003',
+        'LAUCN270370000000004',
+        'LAUCN270370000000005',
+        'LAUCN270370000000006',
+        'LAUCN270530000000003',
+        'LAUCN270530000000004',
+        'LAUCN270530000000005',
+        'LAUCN270530000000006',
+        'LAUCN271230000000003',
+        'LAUCN271230000000004',
+        'LAUCN271230000000005',
+        'LAUCN271230000000006',
+        'LAUCN271390000000003',
+        'LAUCN271390000000004',
+        'LAUCN271390000000005',
+        'LAUCN271390000000006',
+        'LAUCN271630000000003',
+        'LAUCN271630000000004',
+        'LAUCN271630000000005',
+        'LAUCN271630000000006'
+    ]
     thisyear = datetime.date.today().year
     # months_dict is a crosswalk from the bls.gov output to a month alias.
     months_dict = {'M01':'January','M02':'February','M03':'March','M04':'April',
     'M05':'May','M06':'June','M07':'July','M08':'August','M09':'September',
     'M10':'October','M11':'November','M12':'December'}
     headers = {'Content-type': 'application/json'}
-    data = json.dumps({"seriesid":series_list,"startyear":thisyear-9, "endyear":thisyear})
-    p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
-    json_data = json.loads(p.text)
-    outfile = 'laus_data.txt'
-    laus_data = open(outfile,'w')
-    laus_data.write('series,area_type,area,year,period,value,measure,footnote,source,retrieved' + '\n')
-    for series in json_data['Results']['series']:
-        seriesId = series['seriesID']
-        # The measure, area, and area_type variables reference slices of the
-        # SeriesID that are used by bls.gov to identify various attributes of
-        # the Series. These are then translated to aliases with months_dict,
-        # measure_type_dict and area_dict.
-        for item in series['data']:
-            year = item['year']
-            period = months_dict[item['period']]
-            measure = measure_type_dict[seriesId[-2:]]
-            value = item['value']
-            area = area_dict[seriesId[3:18]].replace(',','')
-            area_type = area_type_dict[seriesId[3:5]]
-            retrieved = str(datetime.date.today())
-            footnotes=""
-            for footnote in item['footnotes']:
-                if footnote:
-                    footnotes = footnotes + footnote['text'] + ','
-            laus_data.write(
-            seriesId + ',' +
-            area_type + ',' +
-            area + ',' +
-            year + ',' +
-            period +',' +
-            measure + ',' +
-            value + ',' +
-            footnotes[0:-1] + ',' +
-            'Bureau of Labor Statistics: LAUS,' +
-            retrieved + '\n'
-            )
-    laus_data.close()
+    for y in range(2000:thisyear):
+        data = json.dumps({"seriesid":series_list,"startyear":y, "endyear":y})
+        p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+        json_data = json.loads(p.text)
+        outfile = 'laus_data.txt'
+        laus_data = open(outfile,'w')
+        laus_data.write('series,area_type,area,year,period,value,measure,footnote,source,retrieved' + '\n')
+        for series in json_data['Results']['series']:
+            seriesId = series['seriesID']
+            # The measure, area, and area_type variables reference slices of the
+            # SeriesID that are used by bls.gov to identify various attributes of
+            # the Series. These are then translated to aliases with months_dict,
+            # measure_type_dict and area_dict.
+            for item in series['data']:
+                year = item['year']
+                period = months_dict[item['period']]
+                measure = measure_type_dict[seriesId[-2:]]
+                value = item['value']
+                area = area_dict[seriesId[3:18]].replace(',','')
+                area_type = area_type_dict[seriesId[3:5]]
+                retrieved = str(datetime.date.today())
+                footnotes=""
+                for footnote in item['footnotes']:
+                    if footnote:
+                        footnotes = footnotes + footnote['text'] + ','
+                laus_data.write(
+                seriesId + ',' +
+                area_type + ',' +
+                area + ',' +
+                year + ',' +
+                period +',' +
+                measure + ',' +
+                value + ',' +
+                footnotes[0:-1] + ',' +
+                'Bureau of Labor Statistics: LAUS,' +
+                retrieved + '\n'
+                )
+        laus_data.close()
 
 # Pull the table of area names from bls.gov. This is needed as a crosswalk
 # from SeriesIDs to area name aliases. The resulting dict is then used in the
@@ -91,19 +138,6 @@ def get_measure_type():
             key_clean = key.replace(",","")
             measure_type_dict[key_clean] = a[1]
     return measure_type_dict
-
-# Open the file laus_series.csv in the current directory and convert the
-# contents to a list of SeriesIDs to be requested.
-def get_series():
-    series_list = []
-    series_file = open('laus_series.csv','r')
-    series_lines = series_file.readlines()
-    # Remove the new line character fromm each line before appending to
-    # series_list.
-    for line in series_lines:
-        series_list.append(line.replace('\n',''))
-    series_file.close()
-    return series_list
 
 def main():
     retrieve_bls()
